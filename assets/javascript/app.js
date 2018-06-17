1fca386dc00cbc295a4d4b74b6d42505c4d7c42e
Yeiska <yeiska83@yahoo.com>
2018-06-16 22:54:16 -0400

    setting html

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+
+var question =[
+    "What does the Constitution do?",
+    "The idea of self-government is in the first three words of the Constitution. What are these words?",
+    "What do we call the first ten amendments to the Constitution?",
+    "Who is in charge of the executive branch?",
+    "What are the two parts of the U.S. Congress?*",
+    "We elect a U.S. Senator for how many years?",
+    "Who does a U.S. Senator represent?",
+    "Who signs bills to become laws?",
+    "What do we show loyalty to when we say the Pledge of Allegiance?",
+    "Who wrote the Declaration of Independence?",
+    "When was the Declaration of Independence adopted?",
+    "When was the Constitution written?",
+    "Who is the “Father of Our Country”?",
+    "Before he was President, Eisenhower was a general. What war was he in?",
+    "What did Martin Luther King, Jr. do?",
+];
+var answer = [
+      "sets up the government,defines the government,protects basic rights of Americans",
+      "We the People",
+      "the Bill of Rights",
+      "the President",
+      "the Senate and House (of Representatives)",
+      "six (6)",
+      "all people of the state",
+      "the President",
+      "the United States,the flag",
+      "(Thomas) Jefferson",
+      "July 4, 1776",
+      "1787",
+      "(George) Washington",
+      "World War II",
+      "fought for civil rights, worked for equality for all Americans",
+];
+//on click buttom start load page with game
+//show time remain to answer question and also runing time
+//show question with options' answers to be selected
+//if answer selected correct display text Congratulation!! it is correct for 4 seg
+//add 1 to answers rigth
+//and display next question with answer' option to be selected
+//else display wrong answer selected
+//and show correct answer
+//add 1 to wrong answer
+//display next question to answer
+//if not selected any answers add 1 to unanswered and display correct answer
+//when time remaining = 0 stop game
+//show text All done!! Here how you did:
+//display numers of correct answer:, numers of wrong answer:, and unanswered
+//reset game to star over with buttom
